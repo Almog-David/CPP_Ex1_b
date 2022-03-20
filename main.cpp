94f5e4cf0ed1694a3f042d1b079ba2e681efd7ef
Almog-David <almog.david99@gmail.com>
2022-03-20 22:52:24 +0200

    update task

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "mat.hpp"
+using namespace std;
+using namespace ariel;
+
+string first = mat(7, 9, '$', '!');
+cout << first;
+
+string second = mat(4, 4, '#', '*');
+cout << second;
+
+string third = mat(17, 11, '<', '-');
+cout << third;
